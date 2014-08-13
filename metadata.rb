743abf907267354bcb115c10eb220949ab739ba7
Andreas Fritzler <andreas.fritzler@gmail.com>
2014-08-14 00:07:15 +0200

    Added Apache license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'pi-timecapsule'
 maintainer       'Andreas Fritzler'
 maintainer_email 'andreas.fritzler@gmail.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures pi-timecapsule'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
